82ea2ea3513efbf7f5319c47774013c083c86bc2
linoduver.02402 <linoduver.02402@gmail.com>
2025-04-20 13:48:13 +0000

    Actualizacion en la pantalla de carga, ahora se cargarà el juego e iniciara automaticamente.

--- app/src/main/java/com/example/juegomastermind/LoadingActivity.kt
+++ app/src/main/java/com/example/juegomastermind/LoadingActivity.kt
@@ -4,24 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class LoadingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading)
-
-        val startButton = findViewById<Button>(R.id.startButton)
-        startButton.postDelayed({
-            startButton.visibility = View.VISIBLE
-            startButton.isEnabled = true
-        }, 5000) // 5 segundos de retraso
-
-        startButton.setOnClickListener {
-            startActivity(Intent(this, GameModeActivity::class.java))
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, GameModeActivity::class.java)
+            startActivity(intent)
             finish()
-        }
+        }, 3000) // 3 segundos de retraso
     }
 }
